343556458703e219e36c74ce40535c88e8230c6e
Matias D. Sanchez R. <matiasdsanchezr@gmail.com>
2025-05-06 07:56:57 -0300

    update

--- src/app.mjs
+++ src/app.mjs
@@ -25,6 +25,8 @@ app.use(
   cors({
     origin: config.allowedOrigin,
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'active-profile-id'],
   })
 );
 app.use(cookieParser("TU_SECRETO_ESTABLE"));
